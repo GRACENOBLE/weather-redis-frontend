7fd015c38935daec42816cdb938e852eb149ec94
GRACENOBLE <gracenoble72@gmail.com>
2024-12-11 13:19:54 +0300

    feat(loading)added loading state

--- src/components/foms/weather-form.tsx
+++ src/components/foms/weather-form.tsx
@@ -16,6 +16,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import Container from "../Container";
 import WeatherCards from "../weather-card";
+import Image from "next/image";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -29,18 +30,21 @@ const WeatherForm = () => {
       username: "",
     },
   });
+  const [loading, setLoading] = useState<boolean>(false);
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+    setLoading(true);
 
-    const weather = await GetCurrentWeather();
+    const response = await GetCurrentWeather()
+      .then((weather) => setWeatherData(weather))
+      .then(() => setLoading(false));
 
-    setWeatherData(weather);
+    //console.log(response);
   }
 
   return (
-    <section className="w-full flex flex-col items-center justify-center gap-10 pt-20">
+    <section className="w-full flex flex-col items-center justify-center gap-10 pt-20 px-4">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex gap-8">
           <FormField
@@ -63,13 +67,14 @@ const WeatherForm = () => {
         </form>
       </Form>
       <Container>
-        <div className="border-2 border-white/25 rounded-2xl w-fit p-4 mx-auto bg-slate-400/25 backdrop-blur-md text-white/75">
+        <div className="border-2 border-white/25 rounded-2xl p-4 mx-auto bg-slate-400/25 backdrop-blur-md text-white/75 max-w-[440px]">
           <WeatherCards
             location={weatherData?.timezone}
             temperature={weatherData?.current?.temp}
             description={weatherData?.current?.weather[0]?.description}
             hourly={weatherData?.hourly}
             daily={weatherData?.daily}
+            loading={loading}
           />
         </div>
       </Container>
--- src/components/weather-card.tsx
+++ src/components/weather-card.tsx
@@ -1,4 +1,6 @@
 import { WeatherIcons } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 import timestamp from "unix-timestamp";
 
 const WeatherCards = ({
@@ -7,6 +9,7 @@ const WeatherCards = ({
   description,
   hourly,
   daily,
+  loading,
 }: {
   location: string;
   temperature: string;
@@ -38,29 +41,80 @@ const WeatherCards = ({
       morn: string;
     };
   }[];
+  loading: boolean;
 }) => {
   return (
     <>
-      <div className="bg-black/10 rounded-xl w-[400px] px-4 flex flex-col items-center mx-auto mb-4 py-4">
-        <p>{location}</p>
-        <p className="font-extralight text-8xl pb-4">
-          ~{Math.floor(parseInt(temperature) - 273.15)}°
-        </p>
-        <p className="">{description}</p>
-        <ul className="flex gap-6 pb-6">
-          <p>
-            L : {daily && Math.floor(parseInt(daily[0].temp.min) - 273.15)}°
-          </p>
-          <p>
-            H : {daily && Math.floor(parseInt(daily[0].temp.max) - 273.15)}°
-          </p>
-        </ul>
-        <ul className="flex gap-5 w-full overflow-x-clip">
-          {hourly &&
-            hourly.map(({ dt, weather, temp }) => (
-              <div key={dt} className="flex flex-col gap-2 items-center">
-                <p className="text-sm">
-                  {timestamp.toDate(parseInt(dt)).toUTCString().slice(17, 22)}
+      <div className="bg-black/10 rounded-xl w-full  px-4 h-80 flex flex-col items-center mx-auto mb-4 py-4">
+        {Number.isNaN(parseInt(temperature)) ? (
+          <div className="h-full w-full flex flex-col items-center justify-center gap-4">
+            <Image
+              src={"/images/cloud.svg"}
+              alt={""}
+              width={500}
+              height={500}
+              className={cn("w-40", { "animate-pulse": loading })}
+            />
+            No weather data available yet!
+          </div>
+        ) : (
+          <>
+            <p>{location}</p>
+            <p className="font-extralight text-8xl pb-4">
+              ~{Math.floor(parseInt(temperature) - 273.15)}°
+            </p>
+            <p className="">{description}</p>
+            <ul className="flex gap-6 pb-6">
+              <p>
+                L : {daily && Math.floor(parseInt(daily[0].temp.min) - 273.15)}°
+              </p>
+              <p>
+                H : {daily && Math.floor(parseInt(daily[0].temp.max) - 273.15)}°
+              </p>
+            </ul>
+            <ul className="flex gap-5 w-full overflow-x-clip">
+              {hourly &&
+                hourly.map(({ dt, weather, temp }) => (
+                  <div key={dt} className="flex flex-col gap-2 items-center">
+                    <p className="text-sm">
+                      {timestamp
+                        .toDate(parseInt(dt))
+                        .toUTCString()
+                        .slice(17, 22)}
+                    </p>
+                    <p>
+                      {
+                        WeatherIcons.find(
+                          (icon) =>
+                            icon.dkey == weather[0]?.icon ||
+                            icon.nkey == weather[0]?.icon
+                        )?.value
+                      }
+                    </p>
+                    <p>{Math.floor(parseInt(temp) - 273.15)}°</p>
+                  </div>
+                ))}
+            </ul>
+          </>
+        )}
+      </div>
+
+      <ul className="w-full bg-black/10 rounded-xl px-4 flex flex-col items-center py-4 h-[350px] mx-auto">
+        {loading
+          ? [1, 2, 3, 4, 5, 6, 7, 8].map((_, key) => (
+              <div
+                key={key}
+                className="w-full animate-pulse h-6 bg-black/10 rounded-full my-2"
+              />
+            ))
+          : daily &&
+            daily.map(({ dt, weather, temp }) => (
+              <div
+                key={dt}
+                className="flex justify-between w-full items-center gap-4"
+              >
+                <p className="w-9">
+                  {timestamp.toDate(parseInt(dt)).toUTCString().split(",")[0]}
                 </p>
                 <p>
                   {
@@ -71,38 +125,13 @@ const WeatherCards = ({
                     )?.value
                   }
                 </p>
-                <p>{Math.floor(parseInt(temp) - 273.15)}°</p>
+                <div className="flex items-center gap-4 py-2">
+                  <p>{Math.floor(parseInt(temp?.min) - 273.15)}°</p>
+                  <div className="h-1 w-40 bg-black/10 rounded-full" />
+                  <p>{Math.floor(parseInt(temp?.max) - 273.15)}°</p>
+                </div>
               </div>
             ))}
-        </ul>
-      </div>
-
-      <ul className="w-full bg-black/10 rounded-xl max-w-[600px] px-4 flex flex-col items-center py-4">
-        {daily &&
-          daily.map(({ dt, weather, temp }) => (
-            <div
-              key={dt}
-              className="flex justify-between w-full items-center gap-4"
-            >
-              <p className="w-9">
-                {timestamp.toDate(parseInt(dt)).toUTCString().split(",")[0]}
-              </p>
-              <p>
-                {
-                  WeatherIcons.find(
-                    (icon) =>
-                      icon.dkey == weather[0]?.icon ||
-                      icon.nkey == weather[0]?.icon
-                  )?.value
-                }
-              </p>
-              <div className="flex items-center gap-4 py-2">
-                <p>{Math.floor(parseInt(temp?.min) - 273.15)}°</p>
-                <div className="h-1 w-40 bg-black/10 rounded-full" />
-                <p>{Math.floor(parseInt(temp?.max) - 273.15)}°</p>
-              </div>
-            </div>
-          ))}
       </ul>
     </>
   );
